6eb0fcb7aedd977fb895aca007ec6e06a9c6495d
Roshan <1roshanekka@gmail.com>
2024-01-31 00:10:21 +0530

    Wed Jan 31 00:10:21 IST 2024

---
+++ cpp.CodeForces/A1918.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+    using namespace std;
+    
+    void solve() {
+
+        int n,m;
+        cin>>n>>m;
+        cout << (m/2)*n<<endl;
+        //pick horizontal m
+        //keep minimum so 1x2 ; 
+        // if(m%2==0){ // even
+        //     cout << (m/2)*n<<endl;
+        // }
+        // else{ // odd
+
+        // }
+    }
+
+    signed main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+            int _t;
+            cin >> _t;
+            while(_t--) {
+                solve();
+            }
+    }
